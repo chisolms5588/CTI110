d93bc47acb477a46b5b08e97a4d157c717d52ae8
chisolms5588 <166735356+chisolms5588@users.noreply.github.com>
2024-04-11 11:38:33 -0400

    Add files via upload

---
+++ P1LAB1_ChisolmShamara.py
@@ -0,0 +1,9 @@
+#Shamara Chisolm
+#04/11/24
+#P1LAB1
+#Create a program that allows user to enter their first and last name with a greeting message at the end.
+
+
+firstName = input("Enter your first name:")
+lastName = input("Enter your last name:")
+print("Hello",firstName,lastName,"!","Welcome to CTI-110")
