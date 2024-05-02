fde0cd3360568a222559ba186245b8f2836136c5
chisolms5588 <166735356+chisolms5588@users.noreply.github.com>
2024-05-02 07:31:41 -0400

    Add files via upload

---
+++ P4LAB1_ChisolmShamara.py
@@ -0,0 +1,23 @@
+import turtle
+
+win = turtle.Screen()
+
+sea = turtle.Turtle()
+
+for x in range(4):
+    sea.forward(200)
+    sea.left(90)
+
+sea.penup()
+sea.forward(250)
+sea.pendown()
+
+for x in range(3):
+    sea.forward(250)
+    sea.left(120)
+
+
+
+
+
+
